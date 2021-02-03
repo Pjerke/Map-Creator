f7969d2bc4c49142b6891142120d2f8ef17ecb59
Pieter Gilissen <pieter_gilissen@hotmail.com>
2021-02-03 13:59:06 +0100

    start

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <v-container fluid fill-height>
+        <MapCreator/>
+      </v-container>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MapCreator from './components/MapCreator';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
-</script>
+    MapCreator,
+  },
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  // data: () => ({
+  //   menuItems: [
+  //     { label: 'Locatie', icon: 'mdi-map-marker' },
+  //     { label: 'Stijlen', icon: 'mdi-palette' },
+  //     { label: 'Tekst', icon: 'mdi-format-color-text' }
+  //   ]
+  // }),
+};
+</script>
---
+++ src/components/MapCreator.vue
@@ -0,0 +1,76 @@
+<template>
+    <v-card outlined color="#385F73" class="mx-auto my-auto" width="80vw" height="80vh">
+        <v-container fluid fill-height>
+            <v-row>
+            <!--                        MENU                     -->
+                <v-col cols=2 align-self="center">
+                    <v-card-actions>
+                        <v-navigation-drawer color="#385F73" dark style="height: 70vh">
+                            <v-list
+                                nav
+                            >
+                                <v-list-item
+                                    v-for="item in menuItems"
+                                    :key="item.label"
+                                    link
+                                >
+                                    <v-list-item-icon>
+                                        <v-icon>{{ item.icon }}</v-icon>
+                                    </v-list-item-icon>
+                                    <v-list-item-content>
+                                        <v-list-item-title>{{ item.label }}</v-list-item-title>
+                                    </v-list-item-content>
+                                </v-list-item>
+                            </v-list>
+                        </v-navigation-drawer>
+                    </v-card-actions>
+                </v-col>
+                <!-- <v-divider/> -->
+            <!--                        OPTIONS                     -->
+                <v-col cols=3 align-self="center">
+                    <v-card-text>
+                        <div> TEST </div>
+                    </v-card-text>
+                </v-col>
+            <!--                        MAP                     -->
+                <v-col cols=6 align-self="center">
+                    <div id="map">
+                        <mapbox
+                            access-token="pk.eyJ1IjoicGlua3RydWNrIiwiYSI6ImNrMTU3M3FpZzBwaXEzbnFqNmh2anFhdjUifQ.fSS505uL0Zn-SlQoUglO8w"
+                            :map-options="{
+                                style: 'mapbox://styles/mapbox/light-v9',
+                                center: [-96, 37.8],
+                                zoom: 3,
+                            }"
+                            />
+                    </div>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-card>
+</template>
+
+<script>
+import Mapbox from 'mapbox-gl-vue'
+
+export default {
+  name: 'MapCreator',
+  components: { Mapbox },
+  data: () => ({
+    menuItems: [
+      { label: 'Locatie', icon: 'mdi-map-marker' },
+      { label: 'Stijlen', icon: 'mdi-palette' },
+      { label: 'Tekst', icon: 'mdi-format-color-text' }
+    ]
+  }),
+  methods: {
+  }
+}
+</script>
+
+<style>
+#map {
+  width: 100%;
+  height: 500px;
+}
+</style>
